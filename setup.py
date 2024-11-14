3f88437a8c69f989cc05ec6a5f98e32ad6e2d832
Claus Prüfer <pruefer@webcodex.de>
2024-11-14 13:51:36 +0100

    Update meta data

--- setup.py
+++ setup.py
@@ -1,36 +1,21 @@
 from setuptools import setup
 
 setup(
-    name = 'python-dbpool',
-    version = '0.1.0a0',
+
+    name = 'pgdbpool',
     author = 'Claus Prüfer',
     author_email = 'pruefer@webcodex.de',
-    maintainer = 'Claus Prüfer',
-    description = 'A tiny static postgresql database pool for threaded wsgi webserver (apache2).',
-    license = 'GPLv3',
-    url = 'http://dbpool.python.webcodex.de',
+    description = 'A tiny database de-multiplexer primarily scoped for Web- / Application Server.',
     long_description = open('./README.md').read(),
-
-    classifiers = [
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Database Tools',
-        'License :: OSI Approved :: GPLv3 License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
 
     packages = [
         'pgdbpool'
     ],
+
     package_dir = {
         'pgdbpool': 'src/'
     },
-    install_requires = [
-        'psycopg2'
-    ],
 
-    python_requires = '>=3',
     zip_safe = True
+
 )
