6af53deb6d59327b7491c1c7a6c3eb48b54a7dc8
Claus Prüfer <pruefer@webcodex.de>
2026-01-26 11:05:41 +0100

    Add ESB metadata / processing logic

---
+++ example/01-logical-replication/db-node-rpc/class_mapping.py
@@ -0,0 +1,10 @@
+class_mapping = {
+    'System': 'System',
+    'NetworkTopology': 'NetworkTopology',
+    'NetIPv4': 'NetIPv4',
+    'NetIPv6': 'NetIPv6',
+    'HostNode': 'HostNode',
+    'Database': 'Database',
+    'Table': 'Table',
+    'Column': 'Column'
+}
---
+++ example/01-logical-replication/db-node-rpc/esbconfig.py
@@ -0,0 +1,12 @@
+import_classes = {
+    'service_implementation': [
+        'System',
+        'NetworkTopology',
+        'NetIPv4',
+        'NetIPv6',
+        'HostNode',
+        'Database',
+        'Table',
+        'Column'
+    ]
+}
