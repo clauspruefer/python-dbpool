f97c08d92fbad562e34a5fbb44657fb68e7d0eb3
Claus Prüfer <pruefer@webcodex.de>
2026-02-05 08:28:17 +0100

    Add queries

--- example/01-logical-replication/db-node-rpc/sql_queries.py
+++ example/01-logical-replication/db-node-rpc/sql_queries.py
@@ -4,10 +4,24 @@ ALTER SYSTEM SET max_replication_slots = 10;
 ALTER SYSTEM SET max_wal_senders = 10;
 
 CREATE ROLE replicator WITH REPLICATION LOGIN PASSWORD 'replicator';
+CREATE ROLE testreader WITH LOGIN PASSWORD 'testreader';
+CREATE ROLE testwriter WITH LOGIN PASSWORD 'testwriter';
 '''
 
 create_table = '''
-CREATE TABLE {tablename} (
-{tablecolumns}
+CREATE TABLE {table_name} (
+{table_columns}
 );
+
+GRANT SELECT ON TABLE {table_name} TO replicator;
+GRANT SELECT ON TABLE {table_name} TO testreader;
+GRANT INSERT ON TABLE {table_name} TO testwriter;
+'''
+
+create_publication = '''
+CREATE PUBLICATION {publication_id} FOR TABLE {table_name};
+'''
+
+create_subscription = '''
+CREATE SUBSCRIPTION {subscription_id} CONNECTION 'host={host_ip} dbname=postgres port=5432' PUBLICATION {publication_id} WITH (copy_data = false, origin = none);
 '''
